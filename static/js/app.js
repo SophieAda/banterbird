34d25e5ee2cd6716f4516e85f7231634a8c8878d
SophieAda <sophie.phillips@ascte.org>
2025-04-18 10:51:02 -0500

    Postsfrombackend

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 const username = "admin";
 
-function renderPost(post) {
+function renderPost(post){
     const template = document.getElementById("post-template").content.cloneNode(true);
     template.querySelector(".username").innerText = post.username;
     template.querySelector(".message").innerText = post.message;
@@ -9,14 +9,19 @@ function renderPost(post) {
 
 function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
+    console.log("would post:", message);
     alert("Tweet submitted (not really yet)");
 }
 
-window.onload = () => {
-    const hardcodedPost = {
-        username: "admin",
-        message: "Welcome to Banterbird! This post is hardcoded.",
-    };
-    renderPost(hardcodedPost);
+window.onload = async () => {
+    try{
+        const response = await fetch("/api/posts");
+        const posts = await response.json();
+        posts.forEach(post => {
+            renderPost(post);
+        });
+    } catch (error){
+        console.error("Error:", error);
+    }
+
 };
