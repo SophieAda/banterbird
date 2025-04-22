41a037698ef9aff7ff119202b5bc13dfac068da2
SophieAda <sophie.phillips@ascte.org>
2025-04-22 11:22:28 -0500

    Added post feature (have to refresh)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -14,6 +14,16 @@ def get_posts():
         posts = json.load(file)
     return jsonify(posts)
 
+@app.route('/api/add_post', methods = ['POST'])
+def add_post():
+    new_post = request.get_json()
+    with open('data.json', 'r') as file:
+        posts = json.load(file)
+    posts.insert(0, new_post)
+    with open('data.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+    return jsonify({"status":"success"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
