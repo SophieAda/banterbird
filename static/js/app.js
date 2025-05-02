8e054337fa6e9351d0ab7a17560c28f73175a444
SophieAda <sophie.phillips@ascte.org>
2025-05-02 10:06:27 -0500

    Post without refresh

--- static/js/app.js
+++ static/js/app.js
@@ -48,3 +48,14 @@ window.onload = async () => {
     }
 
 };
+
+setIntervalasync(async () => {
+    try{
+        const response = await fetch("/api/posts");
+        const posts = await response.json();
+        document.getElementById("feed").innerHTML = "";
+        posts.forEach((post) => renderPost(post));
+    } catch(error){
+        console.error("Error fetching posts:", error);
+    }
+    }, 5000);
